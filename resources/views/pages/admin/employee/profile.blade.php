aac7b3b37f8bea1d80ceea8d89eb14fd89f9ef3d
Padz <benpadz08@gmail.com>
2020-07-17 23:35:45 +0800

    Update profile.blade.php

--- resources/views/pages/admin/employee/profile.blade.php
+++ resources/views/pages/admin/employee/profile.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-md-9">
         <div class="row">
-            <div class="col-10">
+            <div class="col-12">
                 <div class="card mb-2">
                     <div class="card-body">
                         <div class="row mb-2">
