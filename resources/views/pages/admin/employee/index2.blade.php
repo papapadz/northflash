68a41b9aef85b1ea5e24f4a228d5b86993070d20
Padz <benpadz08@gmail.com>
2023-03-27 11:19:14 +0800

    Update index2.blade.php

--- resources/views/pages/admin/employee/index2.blade.php
+++ resources/views/pages/admin/employee/index2.blade.php
@@ -32,7 +32,7 @@
             @foreach($employees as $emp)
               <tr class="py-1">
                 <td>
-                  <img src="{{ $emp->img }}" alt="image" />
+                  <img src="{{ $emp->fileImg->url }}" alt="image" />
                 </td>
                 <td>{{ $emp->employee_id }}</td>
                 <td>{{ $emp->last_name }}, {{ $emp->first_name }} {{ $emp->middle_name }}</td>
